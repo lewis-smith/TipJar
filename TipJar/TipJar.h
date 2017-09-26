da9f8ae84e4789eaab6946c7c1beb9be9aa689f0
Lewis Smith <lewisthesmith@gmail.com>
2017-09-26 13:10:18 +0100

    Initial Commit

---
+++ TipJar/TipJar.h
@@ -0,0 +1,19 @@
+//
+//  TipJar.h
+//  TipJar
+//
+//  Created by Lewis Smith on 26/09/2017.
+//  Copyright © 2017 Lewis Makes Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TipJar.
+FOUNDATION_EXPORT double TipJarVersionNumber;
+
+//! Project version string for TipJar.
+FOUNDATION_EXPORT const unsigned char TipJarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TipJar/PublicHeader.h>
+
+
